0f50630471118cb1fad5d55427efcd843d88b46b
mohitgaur <mohitgaur310@gmail.com>
2024-09-15 14:42:26 +0530

    routes

--- src/app/routes/auth.routes.ts
+++ src/app/routes/auth.routes.ts
@@ -1,7 +1,10 @@
 import { Router } from "express";
 import { register } from "../controller/auth.controller";
+import { userSchema } from "../../app/middleware/validator/auth.validator";
+import { validateSchema } from "../../app/middleware/validator/validator";
+
 const router = Router();
 
-router.route("/register").post(register);
+// router.route("/register").post(validateSchema(userSchema), register);
 
 export default router;
