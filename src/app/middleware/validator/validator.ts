cd19629df7c1dfbed3ad3b159aad45158f01f9cc
mohitgaur <mohitgaur310@gmail.com>
2024-09-29 21:02:32 +0530

    changes for the routes

--- src/app/middleware/validator/validator.ts
+++ src/app/middleware/validator/validator.ts
@@ -1,19 +1,16 @@
 import { Request, Response, NextFunction } from "express";
 import { defaults } from "../../../core/constants/validation.interface";
-export const validateSchema = async (
-  schema: any,
-  req: Request,
-  res: Response,
-  next: NextFunction
-) => {
-  const payload = req.body;
-  const { error, value } = schema.validate(payload, defaults);
-  req.body = value;
-  if (error) {
-    return res.status(406).json({
-      error: `Error in user data: ${error.message}`,
-    });
-  } else {
-    next();
-  }
+export const validateSchema = async (schema: any) => {
+  return (req: Request, res: Response, next: NextFunction) => {
+    const payload = req.body;
+    const { error, value } = schema.validate(payload, defaults);
+    req.body = value;
+    if (error) {
+      return res.status(406).json({
+        error: `Error in user data: ${error.message}`,
+      });
+    } else {
+      next();
+    }
+  };
 };
